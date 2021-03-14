9820b6bd74b5258fb7e7c10af537f333649bb342
William Araujo <williamf1f2@gmail.com>
2021-03-14 11:17:18 -0300

    Faz pequena modificação non template

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,11 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const MessageCard = styled.div`
-  width: 25rem;
-  height: 10rem;
-  background-color: #0099ff2f;
-  border-left: 6px solid #0099ff;
-  border-radius: 4px;
-  box-shadow: 4px 4px 5px rgb(0, 0, 0, 0.25);
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-
-  & span {
-    padding-top: 1rem;
-  }
-`
+import WelcomeMessage from '../components/WelcomeMessage'
 
 const Home = () => {
 
   return (
     <>
-      <MessageCard>
-        <h1>Initial React Template</h1>
-        <span>Made by William 😜</span>
-      </MessageCard>
+      <WelcomeMessage />
     </>
   )
 }
